1e67a331ae033c71e67832e7a5f0308875ace127
trisetyabudi2017 <110437412+trisetyabudi2017@users.noreply.github.com>
2025-10-17 10:40:04 +0700

    zodiac versi 1.01
    Penambahan fungsi if

--- Projectzodiac/main.cpp
+++ Projectzodiac/main.cpp
@@ -10,5 +10,24 @@ int main()
         cin>>bln;
         cout<<"Inputkan Tanggal Lahir Anda :";
         cin>>tgl;
+
+            if (bln>0 && bln<=12 )
+                {
+                    if ((bln==12 && tgl>=20 && tgl<=31 )|| (bln==1 && tgl>=1 && tgl<=19))
+                        {
+                            cout<<"Bintang Kejora";
+                        }
+                    else if ((bln==1 && tgl>=20 && tgl<=31 )|| (bln==2 && tgl>=1 && tgl<=19))
+                        {
+                            cout<<"Bintang Tujuh";
+                        }
+
+
+
+
+                }
+            else
+                {cout<<"Bulan Invalid";}
+
     return 0;
 }
