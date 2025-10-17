7dd7ffe1823fb6550fb1fb779edff0e69d1678d1
trisetyabudi2017 <110437412+trisetyabudi2017@users.noreply.github.com>
2025-10-17 10:25:48 +0700

    Zodiac versi 1.00
    Tampilan awal input bulan dan tanggal

---
+++ Projectzodiac/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program Zodiac!" << endl;
+    int bln,tgl;
+        cout<<"Inputkan Bulan Lahir Anda(1-12):";
+        cin>>bln;
+        cout<<"Inputkan Tanggal Lahir Anda :";
+        cin>>tgl;
+    return 0;
+}
